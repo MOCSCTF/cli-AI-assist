24c60202167be4bc6e1bd9941baf17988ceba162
920wY0u20wnCh11d <58168289+920wY0u20wnCh11d@users.noreply.github.com>
2025-04-21 18:11:05 +0800

    revise the loggin conditions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="cli-AI-assist",
-    version="0.1.6",
+    version="0.1.7",
     py_modules=["cli_AI_assist"],
     install_requires=[
         "openai",
